68878024ccae65ada3f3845798ffb338e7220bc5
fumie-kuga <neozoic122@gmail.com>
2022-05-05 16:35:37 +0900

    ログアウトのsetCompleteのために@SessionAttributes("authEmployee")	追記

--- jsysSpringGit/src/main/java/jsysSpring/sales/control/CommonCntroller.java
+++ jsysSpringGit/src/main/java/jsysSpring/sales/control/CommonCntroller.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import jsysSpring.sales.entity.AuthEmployee;
@@ -22,6 +23,7 @@ import jsysSpring.sales.entity.AuthEmployee;
  * @version 1.0.0 2022/03/20
  */
 @Controller
+@SessionAttributes("authEmployee")		//2022/05/05追記
 public class CommonCntroller {
 
 	/** 認証済み従業員情報 */
